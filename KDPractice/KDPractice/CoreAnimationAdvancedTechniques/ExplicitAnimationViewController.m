439fff5060855d9c12134bef472868095c66df6b
“kaidi” <csyibei_120@163.com>
2017-12-19 00:42:18 +0800

    CAKeyframeAnimation

--- KDPractice/KDPractice/CoreAnimationAdvancedTechniques/ExplicitAnimationViewController.m
+++ KDPractice/KDPractice/CoreAnimationAdvancedTechniques/ExplicitAnimationViewController.m
@@ -12,12 +12,16 @@
 
 @property (weak, nonatomic) IBOutlet UIView *aView;
 @property (nonatomic,strong) CALayer *aLayer;
+@property (nonatomic,strong) CALayer *kaiDiLayer;
+@property (nonatomic,strong) UIBezierPath *movePath;
+
 @end
 
 @implementation ExplicitAnimationViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self animationPath];
     // Do any additional setup after loading the view from its nib.
     self.aLayer = [CALayer layer];
     self.aLayer.frame = CGRectMake(0, 0, 100, 100);
@@ -25,7 +29,37 @@
     [self.aView.layer addSublayer:self.aLayer];
 }
 
+- (void)animationPath
+{
+    UIBezierPath *path = [[UIBezierPath alloc] init];
+    [path moveToPoint:CGPointMake(50, 100)];
+    [path addCurveToPoint:CGPointMake(300, 100) controlPoint1:CGPointMake(150, 0) controlPoint2:CGPointMake(200, 200)];
+    self.movePath = path;
+    CAShapeLayer *shape = [CAShapeLayer layer];
+    shape.path = path.CGPath;
+    shape.strokeColor = [UIColor orangeColor].CGColor;
+    shape.fillColor = [UIColor clearColor].CGColor;
+    [self.view.layer addSublayer:shape];
+    _kaiDiLayer = [CALayer layer];
+    _kaiDiLayer.frame = CGRectMake(0, 0, 44, 44);
+    _kaiDiLayer.contents = (__bridge id)[UIImage imageNamed:@"WechatIMG6131"].CGImage;
+    _kaiDiLayer.position = CGPointMake(50, 100);
+    [self.view.layer addSublayer:_kaiDiLayer];
+}
+
+- (void)beginAnimation
+{
+    CAKeyframeAnimation *keyAnimation = [CAKeyframeAnimation animation];
+    keyAnimation.keyPath = @"position";
+    keyAnimation.duration = 3.0f;
+    keyAnimation.path = self.movePath.CGPath;
+    keyAnimation.rotationMode = @"auto";
+    keyAnimation.delegate = self;
+    [_kaiDiLayer addAnimation:keyAnimation forKey:nil];
+}
+
 - (IBAction)changeColorClick:(id)sender {
+     [self beginAnimation];
     CABasicAnimation *ani = [CABasicAnimation animation];
     CGFloat randomRed = arc4random() / (CGFloat)INT_MAX;
     CGFloat randomGreen = arc4random() / (CGFloat)INT_MAX;
@@ -46,8 +80,8 @@
 {
     [CATransaction begin];
     [CATransaction setDisableActions:YES];
-    [CATransaction setAnimationDuration:5.0f];
-    self.aLayer.backgroundColor = (__bridge CGColorRef)anim.toValue;
+//    [CATransaction setAnimationDuration:10.0f];
+//    self.aLayer.backgroundColor = (__bridge CGColorRef)anim.toValue;
 //    self.aLayer.backgroundColor = [UIColor redColor].CGColor;
     [CATransaction commit];
 }
